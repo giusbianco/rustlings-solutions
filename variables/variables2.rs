a12ec245f2628cbaa0c0953791b77bced4ee3872
giusbianco <white951@hotmail.it>
2023-11-12 08:36:46 +0100

    added the solved exercises

---
+++ variables/variables2.rs
@@ -0,0 +1,16 @@
+// variables2.rs
+//
+// Execute `rustlings hint variables2` or use the `hint` watch subcommand for a
+// hint.
+
+
+
+fn main() {
+    //let x = 10;
+    let x = 11;
+    if x == 10 {
+        println!("x is ten!");
+    } else {
+        println!("x is not ten!");
+    }
+}
