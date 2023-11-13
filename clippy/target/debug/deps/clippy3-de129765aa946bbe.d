1d30d503e46077051bf403490aca6f92afe72799
giusbianco <white951@hotmail.it>
2023-11-13 22:16:38 +0100

    added clippy and macros

---
+++ clippy/target/debug/deps/clippy3-de129765aa946bbe.d
@@ -0,0 +1,9 @@
+/home/r06u3/Scrivania/rust-code/rustlings/exercises/clippy/target/debug/deps/clippy3-de129765aa946bbe.rmeta: clippy3.rs Cargo.toml
+
+/home/r06u3/Scrivania/rust-code/rustlings/exercises/clippy/target/debug/deps/clippy3-de129765aa946bbe.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
